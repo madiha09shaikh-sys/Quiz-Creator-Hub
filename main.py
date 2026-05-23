d7514ab58ecd9795ab9af6dfd0b73b6c16daf61b
madiha09shaikh-sys <madiha09shaikh@gmail.com>
2026-05-23 15:19:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,8 @@ app.secret_key = "secret123"
 app.permanent_session_lifetime = timedelta(days=30)
 
 # ================= DATABASE =================
-client = OpenAI(
-    api_key="sk-xxxxxxxx"
-)
 
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 def get_db():
 
     return mysql.connector.connect(
