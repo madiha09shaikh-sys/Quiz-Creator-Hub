2a36e46f4f2486cdda78345e4403056fef85a3d8
madiha09shaikh-sys <madiha09shaikh@gmail.com>
2026-06-08 16:36:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def get_db():
         user=os.getenv("MYSQLUSER"),
         password=os.getenv("MYSQLPASSWORD"),
         database=os.getenv("MYSQLDATABASE"),
-        port=int(os.getenv("MYSQLPORT")),
+        port=int(os.getenv("MYSQLPORT", 3306)),
         autocommit=True
     )
 
